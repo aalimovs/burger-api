eaf1e57988934c8e2a147bf272c24a03eb04d3aa
aalimovs <alex@techwharf.com>
2016-08-07 22:05:15 +0100

    Update /posts _.chain() methods to arrow functions

--- routes/posts.js
+++ routes/posts.js
@@ -37,18 +37,9 @@ internals.handlers.getPosts = {
 				}
 
 				const posts = _.chain(reviews.rows)
-					.sortBy(function (o) {
-						return o.dataValues.createdAt * -1; // sort by createdAt, DESC (latest date on top)
-					})
-					.groupBy(function (o) {
-						return Moment(o.dataValues.createdAt).format('YYYY-MM-DD'); // group by date
-					})
-					.mapValues(function (o) {
-						return _.chain(o)
-							.groupBy(function (o) {
-								return o.dataValues.item; // group by item id inside each date
-							})
-					});
+					.sortBy((o) => o.dataValues.createdAt * -1) // sort by createdAt, DESC (latest date on top)
+					.groupBy((o) => Moment(o.dataValues.createdAt).format('YYYY-MM-DD')) // group by day
+					.mapValues((o) => _.groupBy(o, (x) => x.dataValues.item)); // group by item id inside each date
 
 				reply(posts);
 			})
